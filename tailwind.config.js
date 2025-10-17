e6fc752ceeae6353bc54e997e6126bad01297d85
nicck64 <esgerlinihad2007@gmail.com>
2025-10-17 19:41:00 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  // ... other configs
+  theme: {
+    extend: {
+      colors: {
+        'soft-green': '#A7F3D0', // A nice, light/soft green
+        'purple-400': '#C084FC',
+        'green-400': '#4ADE80',
+      },
+      // ... add the keyframes for animations here if you prefer
+    },
+  },
+};
